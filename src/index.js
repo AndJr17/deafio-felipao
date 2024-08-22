c8c4387934d0fbd892fdcc208003c28e119ac6b9
AndJr17 <juokuzono@gmail.com>
2024-08-22 02:42:32 -0300

    Fim do Projeto

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("Olá Mundo")
+let nome = "Raio Azul"
+let xp = 7545
+
+if(xp <= 1000) {
+    console.log("O herói de nome " + nome + " está no nível de Ferro")
+}else if(xp < 2000) {
+    console.log("O herói de nome " + nome + " está no nível de Bronze")
+}else if(xp < 5000) {
+    console.log("O herói de nome " + nome + " está no nível de Prata")
+}else if(xp < 7000) {
+    console.log("O herói de nome " + nome + " está no nível de Ouro")
+}else if(xp < 8000) {
+    console.log("O herói de nome " + nome + " está no nível de Platina")
+}else if(xp < 9000) {
+    console.log("O herói de nome " + nome + " está no nível de Ascendente")
+}else if(xp < 1000) {
+    console.log("O herói de nome " + nome + " está no nível de Imortal")
+}else if(xp >= 1001) {
+    console.log("O herói de nome " + nome + " está no nível de Radiante")
+}
